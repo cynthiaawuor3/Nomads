98522a2ede4224e3fdf914864ef0d618f42a20e9
Cynthia Awuor Odhiambo <81555062+cynthiaawuor3@users.noreply.github.com>
2024-10-10 10:42:07 +0300

    Update renaming.sh

--- renaming.sh
+++ renaming.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-
-# Define the input directory, output directory, and chromosome renaming file
+##THIS SCRIPT IS ONLY USED IF YOUR VCF FILE CHROMOSOME LABEL SLIGHTLY DIFFER FROM THE SNPEFF DATABASE CHROMOSOME
+#input directory 
 TRIMMED_BASE_DIR="/home/cynthia/NOMADS/2024-08-02_AnGam_CAO/no_sample/20240802_1622_MN30463_FAZ32935_b86dbc88/fastq_pass/trimmed"
+#output directory
 vcf_dir="$TRIMMED_BASE_DIR/vcf/vcf_norm"
+#chromosome renaming file i.e if AgamP43R change to 3R
 rename_file="$TRIMMED_BASE_DIR/newname.txt"
 
-# Loop over all normalized VCF files in the directory
+# renaming the vcf file to match the chromosome labelling in snpeff
 for vcf_file in ${vcf_dir}/barcode*.norm.vcf.gz; do
-  # Extract the base name without the extension
   base_name=$(basename "$vcf_file" .norm.vcf.gz)
-
-  # Define the output file name
   output_file="${vcf_dir}/${base_name}.renamed.vcf.gz"
-
-  # Rename chromosomes in the VCF file using bcftools
-  echo "Renaming chromosomes in $vcf_file using $rename_file"
   bcftools annotate --rename-chrs "$rename_file" -Oz -o "$output_file" "$vcf_file"
-
-  echo "Renamed VCF saved to $output_file"
 done
