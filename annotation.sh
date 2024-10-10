23a92b4e68cc54fc9f8282e32131a0a5faf948d5
Cynthia Awuor Odhiambo <81555062+cynthiaawuor3@users.noreply.github.com>
2024-10-10 11:18:42 +0300

    Update annotation.sh

--- annotation.sh
+++ annotation.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
+# working directory
 TRIMMED_BASE_DIR="/home/cynthia/NOMADS/2024-08-02_AnGam_CAO/no_sample/20240802_1622_MN30463_FAZ32935_b86dbc88/fastq_pass/trimmed"
-# Path to snpEff
+# snpEff path- create variable for the Jar and config file
 SNPEFF_JAR="/home/cynthia/snpEff/snpEff.jar"
-
-# Path to snpEff configuration file
 SNPEFF_CONFIG="/home/cynthia/snpEff/snpEff.config"
-
-# Path to reference genome
+# Reference genome
 REFERENCE_GENOME="Anopheles_gambiae"
 
-# Directory containing VCF files
+#  VCF files Directory
 VCF_DIR="$TRIMMED_BASE_DIR/vcf/vcf_norm"
+# Output directory
 ANNOTATED_DIR="$TRIMMED_BASE_DIR/annotated_vcfs"
-
 mkdir -p "$ANNOTATED_DIR"
+
 # Loop through each .vcf.gz file in the directory
 for VCF_FILE in $VCF_DIR/*.renamed.vcf.gz; do
-    # Extract the base name of the file without the extension
     BASE_NAME=$(basename "$VCF_FILE" .renamed.vcf.gz)
-    
-    # Define the output file name
     OUTPUT_FILE="$TRIMMED_BASE_DIR/annotated_vcfs/${BASE_NAME}.annotated.vcf"
     ZIP_FILE="$TRIMMED_BASE_DIR/annotated_vcfs/${BASE_NAME}.annotated.vcf.gz"
-    
-    # Run snpEff command
+    # Run snpEff 
     java -jar "$SNPEFF_JAR" ann -noLog -noStats -no-downstream -no-upstream -no-utr -c "$SNPEFF_CONFIG" -o vcf "$REFERENCE_GENOME" "$VCF_FILE" > "$OUTPUT_FILE"
-    
     bgzip -c "$OUTPUT_FILE" > "$ZIP_FILE"
-
     bcftools index  "$ZIP_FILE" 
-
-# Optionally, print the progress
-    echo "Annotated $VCF_FILE -> $ZIP_FILE"
 done
 
+#Change directory to the annotated files directory and merge all the vcf files for downstream analysis
 cd "$ANNOTATED_DIR" 
 bcftools merge *.annotated.vcf.gz -Oz -o Ag_merged_variants.vcf.gz
  
